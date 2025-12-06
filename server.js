b4b4176ba529ea0e295e5f3b3f777ee774b6c850
chandru12588 <chandru.jerry@gmail.com>
2025-12-06 23:49:26 +0530

    updaded serverjs

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:5175",
       "https://wrongturn-frontend.vercel.app",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
